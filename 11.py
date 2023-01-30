d697974f8f1e0da61cb97d81603970606a5f5cf9
Crazy_MoT <matgol1998@gmail.com>
2023-01-30 23:23:48 +0530

    Lecture finish

---
+++ 11.py
@@ -0,0 +1,33 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/"
+browser = webdriver.Chrome()
+
+@pytest.fixture(scope="class")
+def prepare_faces():
+    print("^_^", "\n")
+    yield
+    print(":3", "\n")
+
+
+@pytest.fixture()
+def very_important_fixture():
+    print(":)", "\n")
+
+
+@pytest.fixture(autouse=True)
+def print_smiling_faces():
+    print(":-Р", "\n")
+
+
+class TestMainPage1():
+    def test_guest_should_see_login_link(self, browser):
+        # не передаём как параметр фикстуру prepare_data, но она все равно выполняется
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")

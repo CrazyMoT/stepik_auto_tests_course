a2a9173a3436a429214b3568d9aeca8bcc6ceb25
Crazy_MoT <matgol1998@gmail.com>
2023-02-01 02:31:52 +0530

    Lecture finish

--- conftest.py
+++ conftest.py
@@ -1,35 +1,16 @@
 import pytest
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
-                     help="Choose browser: '--browser_name=chrome' or '--browser_name=firefox'")
-
     parser.addoption('--language', action='store', default=None,
-                     help="Choose language: '--language=en' or '--language=ru'")(scope="function")
+                     help="Choose language: en, es, fr, etc.")
 
-
-def r_browser(request):
-    browser_name = request.config.getoption("browser_name")
+@pytest.fixture(scope="function")
+def browser(request):
     user_language = request.config.getoption("language")
-
     options = Options()
-    options.add_experimental_option(
-        'prefs', {'intl.accept_languages': user_language})
-
-    options_firefox = OptionsFirefox()
-    options_firefox.set_preference("intl.accept_languages", user_language)
-
-    r_browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        r_browser = webdriver.Chrome(options=options)
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        r_browser = webdriver.Firefox(options=options_firefox)
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
-    yield r_browser
-    print("\nquit browser..")
-    r_browser.quit()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,14 +1,12 @@
+import time
+
 from selenium.webdriver.chrome.options import Options
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-@pytest.fixture
-def test_guest_should_see_login_link(browser, language):
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(options=options)
-    link = "http://selenium1py.pythonanywhere.com/{}/".format(language)
-    browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, "#login_link")
 
+def test_add_to_cart_button_exists(browser):
+    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    add_to_cart_button = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
+    assert add_to_cart_button, "Add to cart button is not found"
